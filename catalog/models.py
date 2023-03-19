d004701d08809ce2f221c733a0a4353af04578c3
I.Muratov <ivanmuratov.2016@gmail.com>
2023-03-19 21:49:52 +0200

    models created

--- catalog/models.py
+++ catalog/models.py
@@ -1,1 +1,32 @@
-# Create your models here.
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Stuff(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    stuff = models.ManyToManyField(Stuff)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.OneToOneField(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
