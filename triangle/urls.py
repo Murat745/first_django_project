00e4eaca9ba5838120a72ce265852584c461c1be
I.Muratov <ivanmuratov.2016@gmail.com>
2023-03-26 22:40:46 +0300

    person forms, views, models modified

--- triangle/urls.py
+++ triangle/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import person, update_person
 
 app_name = 'triangle'
 urlpatterns = [
     path('triangle/', views.triangle, name='triangle'),
+    path('person', person, name='person'),
+    path('person/<int:pk>', update_person, name='update_person'),
 ]
